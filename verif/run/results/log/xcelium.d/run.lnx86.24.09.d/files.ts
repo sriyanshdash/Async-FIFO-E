8ab7e0e2aa63118580acc7a95f58b79a9506b96c
Sriyansh Dash <sdash@mqcomp6.marqueesemi.com>
2026-03-16 16:55:49 +0530

    Working correctly - all 9 test cases in one file

---
+++ verif/run/results/log/xcelium.d/run.lnx86.24.09.d/files.ts
@@ -0,0 +1,6 @@
+1773660043 /scratch/users/sdash/Async-FIFO-/rtl/synchronizer.sv
+1773660043 /scratch/users/sdash/Async-FIFO-/rtl/fifo_memory.sv
+1773660043 /scratch/users/sdash/Async-FIFO-/rtl/wrptr_handler.sv
+1773660043 /scratch/users/sdash/Async-FIFO-/rtl/rdptr_handler.sv
+1773660043 /scratch/users/sdash/Async-FIFO-/rtl/fifo_top.sv
+1773660043 /scratch/users/sdash/Async-FIFO-/verif/tb/fifo_tb_top.sv
